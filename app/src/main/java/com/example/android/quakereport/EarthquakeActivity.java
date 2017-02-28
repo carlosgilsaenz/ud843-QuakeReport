27d039e36e63b1201bbfb5d607dc3d45b0ba2dd6
John Doe <john.doe@example.com>
2017-02-27 23:46:41 -0800

    1.2 complete AsynTask & HttpConnection

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,33 +15,81 @@
  */
 package com.example.android.quakereport;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    EarthquakeAdapter mAdapter;
+
+    private static final String USGS_REQUEST_URL  = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<SeismicData> seismicData = QueryUtils.extractEarthquakes();
-
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of seismicData
-        EarthquakeAdapter adapter = new EarthquakeAdapter(this, seismicData);
+        mAdapter = new EarthquakeAdapter(this, new ArrayList<SeismicData>());
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+        earthquakeListView.setAdapter(mAdapter);
+
+        //initiate background thread
+        new EarthquakeAsyncTask().execute(USGS_REQUEST_URL);
+    }
+
+    /**
+     * {@link AsyncTask} to perform the network request on a background thread, and then
+     * update the UI with the first earthquake in the response.
+     */
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List <SeismicData>> {
+
+        @Override
+        protected List<SeismicData> doInBackground(String... strings) {
+            // Create URL object
+            URL url = QueryUtils.createUrl(strings[0]);
+
+            // Perform HTTP request to the URL and receive a JSON response back
+            String jsonResponse = "";
+            try {
+                jsonResponse = QueryUtils.makeHttpRequest(url);
+            } catch (IOException e) {
+                // TODO Handle the IOException
+                Log.e(LOG_TAG, "Problem with making HTTP connection");
+
+            }
+
+            // Extract relevant fields from the JSON response and create an {@link Event} object
+            ArrayList<SeismicData> earthquakes = QueryUtils.extractFromJson(jsonResponse);
+
+            // Return the {@link Event} object as the result fo the {@link TsunamiAsyncTask}
+            return earthquakes;
+        }
+
+        @Override
+        protected void onPostExecute(List<SeismicData> seismicData) {
+            mAdapter.clear();
+
+            mAdapter.addAll(seismicData);
+
+        }
     }
 }
